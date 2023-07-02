dab12e28abaa05447d4e0d8dfcb0bd4ebc901ff8
gibbykaro <elecgibbyk@cox.net>
2023-07-02 12:42:50 -0500

    New buuild Zabbix ssh agent_V1.0

---
+++ ssh_zabbix_agent.py
@@ -0,0 +1,12 @@
+import paramiko
+
+
+ssh_client = paramiko.SSHClient()
+
+ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+ssh_client.connect('host')
+
+stout = ssh_client.exec_command('')
+
+print(stout)
